41bee450054781fb8de1f7857774cfb1d0025d38
snishtha20 <snishtha2006@gmail.com>
2025-12-27 16:37:08 +0530

    Added first Java array program

---
+++ Arrays/HelloArray.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+class HelloArray{
+    public static void main(String args[]){
+        int a[] = new int[5];
+        Scanner u = new Scanner(System.in);
+        a[0] = u.nextInt();
+        a[1] = u.nextInt();
+        a[2] = u.nextInt();
+        a[3] = u.nextInt();
+        a[4] = u.nextInt();
+
+        System.out.print("[ ");
+        for(int i=0; i<a.length; i++){
+            System.out.print(a[i] + " ");
+        }
+        System.out.print("]");
+    }
+}
