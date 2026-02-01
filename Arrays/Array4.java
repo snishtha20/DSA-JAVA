47dd73f77d49b68d8d97d14ea66096b33a019c6b
snishtha20 <snishtha2006@gmail.com>
2026-02-01 12:05:55 +0530

    Binary Search Code

--- Arrays/Array4.java
+++ Arrays/Array4.java
@@ -1,19 +1,26 @@
+// Binary Search
 class Array4 {
-    static int Pair(int nums[]){
-        int tp = 0;
-        for(int i=0; i<nums.length; i++){
-            for(int j=i+1; j<nums.length; j++){
-                System.out.print( "(" + nums[i]+ "," + nums[j] + ")");
-                tp++;
+    static int BinarySearch(int nums[], int key){
+        int start = 0;
+        int end = nums.length-1;
+        while(start<=end){
+            int mid = (start+end)/2;
+            if(nums[mid] == key){
+                return mid;
             }
-            System.out.println();
+            else if(nums[mid] < key){ //right
+                start = mid +1;
+            }
+            else{    //left
+                end = mid -1;
+            }
         }
-        System.out.println("Total pairs:" + tp);
-        return 0;
+        return -1;
     }
     public static void main(String args[]){
-        int nums[]= {2,4,6,8,10};
-
-        Pair(nums);
-    }    
+        int nums[] = {2,4,6,8,10,12,14};
+        int key = 10;
+        
+        System.out.println("Index of key found at: " + BinarySearch(nums,key));
+    }
 }
