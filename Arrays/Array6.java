5447478da9bcd052c3c065b738e6b7e0d2afa112
snishtha20 <snishtha2006@gmail.com>
2026-02-01 17:05:23 +0530

    Pairs in array

---
+++ Arrays/Array6.java
@@ -0,0 +1,20 @@
+// Print pairs
+class Array6 {
+    static int Pair(int nums[]){
+        int tp = 0;
+        for(int i=0; i<nums.length; i++){
+            for(int j=i+1; j<nums.length; j++){
+                System.out.print( "(" + nums[i]+ "," + nums[j] + ")");
+                tp++;
+            }
+            System.out.println();
+        }
+        System.out.println("Total pairs:" + tp);
+        return 0;
+    }
+    public static void main(String args[]){
+        int nums[]= {2,4,6,8,10};
+
+        Pair(nums);
+    }    
+}
