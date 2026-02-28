f173fd836192c70e24fc5bb05cb5838beef13231
snishtha20 <snishtha2006@gmail.com>
2026-03-01 01:54:32 +0530

    Solved LC_54 Spiral Matrix

---
+++ Leetcode/spiralMatrix.java
@@ -0,0 +1,42 @@
+// LeetCode Problem: Spiral Matrix
+// Difficulty: Easy
+// Topic: 2D Arrays
+
+class Solution {
+    public List<Integer> spiralOrder(int[][] matrix) {
+        List<Integer> result = new ArrayList<>();
+        int startrow = 0;
+        int startcol = 0;
+        int endrow = matrix.length-1;
+        int endcol = matrix[0].length-1;
+        while(startrow <= endrow && startcol <= endcol){
+            for(int j=startcol; j<=endcol; j++){
+                result.add(matrix[startrow][j]);
+            }
+            for(int i=startrow+1; i<= endrow; i++){
+                result.add(matrix[i][endcol]);
+            }
+            for(int j=endcol-1; j>=startcol; j--){
+                if(startrow == endrow){
+                    break;
+                }
+                else{
+                    result.add(matrix[endrow][j]);
+                }
+            }
+            for(int i=endrow-1; i>= startrow+1; i--){
+                if(startcol == endcol){
+                    break;
+                }
+                else{
+                    result.add(matrix[i][startcol]);
+                }
+            }
+            startrow++;
+            startcol++;
+            endrow--;
+            endcol--;
+        } 
+        return result;
+    }
+}
